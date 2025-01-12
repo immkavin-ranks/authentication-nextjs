b200d094685430df005bcb7825cc49d8af3021df
immkavin-ranks <immkavin.ofcl@gmail.com>
2025-01-12 16:36:57 +0530

    Implement session creation with database storage and UUID for user identification

--- src/app/lib/session.ts
+++ src/app/lib/session.ts
@@ -2,9 +2,15 @@ import "server-only";
 import { SignJWT, jwtVerify } from "jose";
 import { SessionPayload } from "@/app/lib/definitions";
 import { cookies } from "next/headers";
+import postgres from "postgres";
+import { drizzle } from "drizzle-orm/postgres-js";
+import { sessions } from "@/db/schema";
 
 const secretKey = process.env.SESSION_SECRET;
 const encodedKey = new TextEncoder().encode(secretKey);
+
+const client = postgres(process.env.DATABASE_URL!);
+const db = drizzle({ client });
 
 export async function encrypt(payload: SessionPayload) {
   return new SignJWT(payload)
@@ -25,9 +31,25 @@ export async function decrypt(session: string | undefined = "") {
   }
 }
 
-export async function createSession(userId: string) {
+export async function createSession(uuid: string) {
   const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
-  const session = await encrypt({ userId, expiresAt });
+
+  // 1. Create a session in the database
+  const data = await db
+    .insert(sessions)
+    .values({
+      user_id: uuid,
+      expires_at: expiresAt,
+    })
+    // Return the session ID
+    .returning({ id: sessions.id });
+
+  const sessionId = data[0].id;
+
+  // 2. Encrypt the session ID
+  const session = await encrypt({ sessionId, expiresAt });
+
+  // 3. Store the session in cookies for optimistic auth checks
   const cookieStore = await cookies();
 
   cookieStore.set("session", session, {
