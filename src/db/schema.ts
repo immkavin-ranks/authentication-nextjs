e89180375702c335540aea41fc3832cc55ca7a32
immkavin-ranks <immkavin.ofcl@gmail.com>
2025-01-12 15:01:24 +0530

    Add user_id field with UUID type to users table schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,9 @@
-import { bigint, pgTable, text, } from "drizzle-orm/pg-core";
+import { bigint, pgTable, text, uuid, } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
   id: bigint({ mode: "number" }).primaryKey().generatedAlwaysAsIdentity(),
   name: text().notNull(),
   email: text().notNull().unique(),
   password: text().notNull(),
+  user_id: uuid().primaryKey().unique(),
 });
