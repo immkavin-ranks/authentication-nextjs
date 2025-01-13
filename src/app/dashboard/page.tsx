56912a4205dcced870de01a1c3bed0aebfcff8d5
immkavin-ranks <immkavin.ofcl@gmail.com>
2025-01-13 10:57:11 +0530

    Implement role-based dashboard rendering with session verification

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,18 @@
-export default function Dashboard() {
-  return <h1>Dashboard page</h1>;
+import { verifySession } from "@/app/lib/dal";
+import AdminDashboard from "@/components/admin-dashboard";
+import UserDashboard from "@/components/user-dashboard";
+import { redirect } from "next/navigation";
+
+
+export default async function Dashboard() {
+  const session = await verifySession();
+  const userRole = session?.user?.role; // Assuming 'role' is part of the session object
+
+  if (userRole === "admin") {
+    return <AdminDashboard />;
+  } else if (userRole === "user") {
+    return <UserDashboard />;
+  } else {
+    redirect("/login");
+  }
 }
